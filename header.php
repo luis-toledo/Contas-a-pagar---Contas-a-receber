4dfa2f066fced3a523c65664bea182c2c4af605a
Luis Fernando Toledo <116091382+luis-toledo@users.noreply.github.com>
2023-05-24 13:32:10 -0400

    Upadate listaRegistros.php
    Ajuste de estrutura HTML, criação de um novo arquivo CSS e estilizações.

--- header.php
+++ header.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Caixa</title>
     <link rel="stylesheet" href="./CSS/style.css">
+    <link rel="stylesheet" href="./CSS/registros.css">
 </head>
 
 <body>
--- listaRegistros.php
+++ listaRegistros.php
@@ -7,59 +7,62 @@
     $executaSoma = $querySoma->execute();
     $soma = $querySoma->fetch(PDO::FETCH_ASSOC);
     ?>
-        <article class="novos-lancamentos">
-            <div class="secao-novos-lancamentos">
-                <div class="form-lancamentos">
-                    <h2 class="titulo-lancamento">TESTE</h2>
-                    <div class="conteudo-lancamentos-tabela">
-                        <table>
-                            <tr>
-                                <th>Descrição</th>
-                                <th>Valor</th>
-                                <th>Tipo</th>
-                                <th>Data Entrada</th>
-                                <th>Data Vencimento</th>
-                                <th>Data Pagamento</th>
+        <article class="lista-de-registros">
+            <div class="lista-de-registros-conteudo">
+                <h2 class="titulo-lista">TESTE</h2>
+                <div class="secao-tabela">
+                    <table class="tabela-lista">
+                        <thead class="cabecalho">
+                            <tr class="linha-principal">
+                                <th class="coluna-principal">Descrição</th>
+                                <th class="coluna-principal">Valor</th>
+                                <th class="coluna-principal">Tipo</th>
+                                <th class="coluna-principal">Data Entrada</th>
+                                <th class="coluna-principal">Data Vencimento</th>
+                                <th class="coluna-principal">Data Pagamento</th>
+                                <th class="coluna-principal-acao">Ações</th>
                             </tr>
+                        </thead>
+                        <tbody class="corpo">
                             <?php 
                             while($financeiros = $query->fetch(PDO::FETCH_ASSOC)) { 
                             ?>
-                                <tr>
-                                    <td>
-                                        <?$financeiros['descricao']; ?>
+                                <tr class="linha-registro"> 
+                                    <td class="coluna-registro"> 
+                                        <?php echo $financeiros['descricao']; ?>
                                     </td>
-                                    <td>
-                                        <?$financeiros['valor']; ?>
+                                    <td class="coluna-registro">
+                                        <?php echo 'R$ ' . $financeiros['valor']; ?>
                                     </td>
-                                    <td>
-                                        <?$financeiros['tipo']; ?>
+                                    <td class="coluna-registro">
+                                        <?php echo $financeiros['tipo']; ?>
                                     </td>
-                                    <td>
+                                    <td class="coluna-registro">
                                         <?php $dataEntrada = date_create( $financeiros['data_cadastro']);?>
                                         <?=  date_format($dataEntrada, 'd/m/Y');?>
                                     </td>
-                                    <td>
+                                    <td class="coluna-registro">
                                         <?php $dataVencimento = date_create( $financeiros['data_vencimento']);?>
                                         <?=  date_format($dataVencimento, 'd/m/Y');?>
                                     </td>
-                                    <td>
+                                    <td class="coluna-registro">
                                         <?php $dataPagamento = date_create( $financeiros['data_pagamento']);?>
                                         <?=  date_format($dataPagamento, 'd/m/Y');?>
                                     </td>
-                                    <td>
-                                        <a href="editar.php?id=<?=$financeiros['id']?>">Editar</a> 
-                                    </td>
-                                    <td>
-                                        <a href="excluir.php?id=<?=$financeiros['id']?>">Excluir</a>
+                                    <td class="coluna-acoes">
+                                        <div class="buttons-acoes">
+                                            <a class="acao" href="editar.php?id=<?=$financeiros['id']?>">Editar</a>
+                                            <a class="acao" href="excluir.php?id=<?=$financeiros['id']?>">Excluir</a> 
+                                        </div>
                                     </td>
                                 </tr>
                             <?php } ?>
-                        </table>
-                    </div>
-                    <div class="conteudo-lancamentos-totais">
-                        <label for="">Totais:</label>
-                        <output class="saida-saldo" for="" name="total"><?= $soma['total']; ?></output>
-                    </div>
+                        </tbody>
+                    </table>
+                </div>
+                <div class="conteudo-lancamentos-totais">
+                    <label class="total-registro" for="">Totais:</label>
+                    <output class="saida-saldo" for="" name="total"><?= 'R$ ' . $soma['total']; ?></output>
                 </div>
             </div>
         </article>
