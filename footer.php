c8bb36c22307a1ae38e85ced3a6b0244b9cbb899
Luis Fernando Toledo <116091382+luisDUScodigo@users.noreply.github.com>
2023-05-27 15:51:20 -0400

    Update footer.php

--- footer.php
+++ footer.php
@@ -5,8 +5,6 @@
         <div class="rodape-conteudo rodape-fonte">
             <p class="rodape-titulo">Desenvolvido por</p>
             <a class="rodape-link rodape-fonte" href="https://www.linkedin.com/in/luisfernandotoledo/">Luís Fernando</a>
-            <p class="rodape-fonte">&</p>
-            <a class="rodape-link rodape-fonte" href="https://www.linkedin.com/in/eduardo-santos-073305202/">Eduardo</a>
         </div>
     </footer>
 </body>
